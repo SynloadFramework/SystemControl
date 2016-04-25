ed16c0549d46721cbb4b3624aa4f2d061a552707
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-04-25 00:38:58 -0700

    initial

---
+++ src/main/java/tech/synframe/template/SystemControl.java
@@ -0,0 +1,23 @@
+package tech.synframe.template;
+
+import com.synload.framework.Log;
+import com.synload.framework.modules.ModuleClass;
+import com.synload.framework.modules.annotations.Module;
+import com.synload.framework.modules.annotations.Module.LogLevel;
+
+/*
+ * Module class (requires both the annotation and the extending of the class)
+ */
+@Module(author = "Nathaniel Davidson", depend = { "" }, log = LogLevel.INFO, name = "System Control", version = "1.0")
+public class SystemControl extends ModuleClass {
+	@Override
+	public void initialize() {
+		// Sent when the module is run (after module elements loaded)
+		Log.info("Loaded System Control", SystemControl.class);
+	}
+	@Override
+	public void crossTalk(Object... obj) {
+		
+	}
+
+}
---
+++ src/main/java/tech/synframe/template/handlers/http/Index.java
@@ -0,0 +1,26 @@
+package tech.synframe.template.handlers.http;
+
+import java.io.IOException;
+
+import com.synload.framework.Log;
+import com.synload.framework.http.HttpRequest;
+import com.synload.framework.http.annotations.Get;
+import com.synload.framework.http.annotations.MimeType;
+import com.synload.framework.http.annotations.Post;
+import com.synload.framework.modules.ModuleLoader;
+
+/*
+ * Class wrapping http response methods
+ */
+public class Index {
+	@Get("/") // HTTP get annotation ( registers as get request http://yoursite.com:8080/template )
+	@MimeType("text/html") // returns OK from server and the mimetype content type
+	public void gHtml(HttpRequest hr){
+		hr.getResponse().setContentLength(ModuleLoader.resources.get("systemcontrol").get("index.html").length);
+		try {
+			hr.getResponse().getOutputStream().write(ModuleLoader.resources.get("systemcontrol").get("index.html"));
+		} catch (IOException e) {
+			Log.info(e.getMessage(), Index.class);
+		}
+	}
+}

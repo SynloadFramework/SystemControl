80e12d71c77538e164f1d2047316e202484f492e
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-05-07 13:15:41 -0700

    Added index trigger to login form or to dashboard page

---
+++ src/main/java/tech/synframe/systemcontrol/elements/PageWrapper.java
@@ -0,0 +1,34 @@
+package tech.synframe.systemcontrol.elements;
+
+import com.synload.framework.handlers.Response;
+import com.synload.framework.modules.ModuleLoader;
+import com.synload.framework.ws.WSHandler;
+import tech.synframe.systemcontrol.models.PendingAction;
+import tech.synframe.systemcontrol.models.Project;
+import tech.synframe.systemcontrol.models.User;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+/**
+ * Created by Nathaniel on 5/7/2016.
+ */
+public class PageWrapper extends Response {
+    public User user;
+    public PageWrapper(WSHandler session, List<String> templateCache, User u){
+        this.user = u;
+        this.setTemplateId("wrap-sc"); // set the template id (stored client side)
+        if(!templateCache.contains(this.getTemplateId())){ // check to see if the client has this template
+            try {
+                this.setTemplate(this.getTemplate(new String(ModuleLoader.resources.get("systemcontrol").get("templates/wrapper.html"),"UTF-8"))); // retrieve template from memory
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        this.setParent("body"); // parent element, css selector
+        this.setParentTemplate("index"); // if element is not found, which template contains it. client side sends (method="get",action=ParentTemplate)
+        this.setAction("alone"); // how to transition the element to the new content
+        this.setPageId("systemControl");
+        this.setPageTitle("System Control");
+    }
+}
--- src/main/java/tech/synframe/systemcontrol/handlers/ws/IndexPage.java
+++ src/main/java/tech/synframe/systemcontrol/handlers/ws/IndexPage.java
@@ -2,15 +2,29 @@ package tech.synframe.systemcontrol.handlers.ws;
 
 import com.synload.eventsystem.events.RequestEvent;
 import com.synload.framework.ws.annotations.WSEvent;
+import tech.synframe.systemcontrol.elements.PageWrapper;
+import tech.synframe.systemcontrol.elements.user.LoginForm;
+import tech.synframe.systemcontrol.models.User;
 
 public class IndexPage {
 	@WSEvent(method = "get", action = "index", description = "default page that loads", enabled = true, name = "Template Index")
 	public void gIndex(RequestEvent e){
-		/*e.getSession().send(
-			new TestElement(
-				e.getSession(),
-				e.getRequest().getTemplateCache()
-			)
-		);*/
+		if(e.getSession().getSessionData().containsKey("user")){ // logged in?
+			User user = (User) e.getSession().getSessionData().get("user");
+			e.getSession().send(
+				new PageWrapper(
+					e.getSession(),
+					e.getRequest().getTemplateCache(),
+					user
+				)
+			);
+		}else{ // not logged in
+			e.getSession().send(
+				new LoginForm(
+					e.getSession(),
+					e.getRequest().getTemplateCache()
+				)
+			);
+		}
 	}
 }

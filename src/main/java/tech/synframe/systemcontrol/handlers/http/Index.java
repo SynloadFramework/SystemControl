8a987b1a4bc2caa2d18d5fbfa7461f80d05c1937
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-05-14 02:14:31 -0700

    update index.java to synframe 1.4.1 moduleresource handling

--- src/main/java/tech/synframe/systemcontrol/handlers/http/Index.java
+++ src/main/java/tech/synframe/systemcontrol/handlers/http/Index.java
@@ -7,6 +7,7 @@ import com.synload.framework.http.HttpRequest;
 import com.synload.framework.http.annotations.Get;
 import com.synload.framework.http.annotations.MimeType;
 import com.synload.framework.modules.ModuleLoader;
+import com.synload.framework.modules.ModuleResource;
 
 /*
  * Class wrapping http response methods
@@ -15,9 +16,9 @@ public class Index {
 	@Get("/") // HTTP get annotation ( registers as get request http://yoursite.com:8080/template )
 	@MimeType("text/html") // returns OK from server and the mimetype content type
 	public void gHtml(HttpRequest hr){
-		hr.getResponse().setContentLength(ModuleLoader.resources.get("systemcontrol").get("templates/main.html").length);
+		hr.getResponse().setContentLength(ModuleResource.get("systemcontrol","templates/main.html").length);
 		try {
-			hr.getResponse().getOutputStream().write(ModuleLoader.resources.get("systemcontrol").get("templates/main.html"));
+			hr.getResponse().getOutputStream().write(ModuleResource.get("systemcontrol","templates/main.html"));
 		} catch (IOException e) {
 			Log.info(e.getMessage(), Index.class);
 		}

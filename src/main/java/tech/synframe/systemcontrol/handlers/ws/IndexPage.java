ea748bfba8193591b15c661615244422dec20812
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-05-23 00:56:22 -0700

    updated resources

--- src/main/java/tech/synframe/systemcontrol/handlers/ws/IndexPage.java
+++ src/main/java/tech/synframe/systemcontrol/handlers/ws/IndexPage.java
@@ -13,7 +13,7 @@ public class IndexPage {
 	public void gIndex(RequestEvent e){
 		if(e.getSession().getSessionData().containsKey("user")){ // logged in?
 			User user = (User) e.getSession().getSessionData().get("user");
-			e.getSession().send(
+			e.respond(
 				new PageWrapper(
 					e.getSession(),
 					e.getRequest().getTemplateCache(),

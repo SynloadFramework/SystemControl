4d710aa5e04b7f5b8866e3d2d275eada2b30b254
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2016-05-27 16:41:59 -0600

    debug info

--- src/main/java/tech/synframe/systemcontrol/utils/NewVersionCheck.java
+++ src/main/java/tech/synframe/systemcontrol/utils/NewVersionCheck.java
@@ -77,7 +77,7 @@ public class NewVersionCheck implements Runnable {
                 apiRequest = apiRequest + "/";
             }
             apiRequest=apiRequest+"api/json?tree=builds[number]";
-            //Log.info(Unirest.post(apiRequest).asString().getBody(),NewVersionCheck.class);
+            Log.info(Unirest.post(apiRequest).asString().getBody(),NewVersionCheck.class);
             HttpResponse<JsonNode> response = Unirest.post(apiRequest).asJson();
             if(response.getBody().getObject().has("builds")){
                 if(response.getBody().getObject().getJSONArray("builds").length()>0){
@@ -100,7 +100,7 @@ public class NewVersionCheck implements Runnable {
         }
         apiRequest=apiRequest+build+"/api/json";
         try {
-            //Log.info(Unirest.post(apiRequest).asString().getBody(),NewVersionCheck.class);
+            Log.info(Unirest.post(apiRequest).asString().getBody(),NewVersionCheck.class);
             return Unirest.post(apiRequest).asJson().getBody();
         }catch (Exception e){
             e.printStackTrace();
